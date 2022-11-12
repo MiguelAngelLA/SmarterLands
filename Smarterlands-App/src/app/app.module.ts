d0a519d6bb0d93f2df09c05b5ddfa548d6192ad4
Gtorrecillas103 <gtorrecillas103@gmail.com>
2022-11-11 18:26:07 -0800

    primeng añadido

--- Smarterlands-App/src/app/app.module.ts
+++ Smarterlands-App/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { PrimengModule } from './primeng/primeng.module';
 
 import { AppComponent } from './app.component';
 
