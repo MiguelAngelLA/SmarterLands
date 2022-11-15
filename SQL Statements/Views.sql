c81760f27ffc7bd4734b9f4518fb6dd21ee2778c
Samtray <sammychurro@gmail.com>
2022-11-14 18:40:30 -0800

    a

--- SQL Statements/Views.sql
+++ SQL Statements/Views.sql
@@ -5,12 +5,6 @@ create view VW_GetAllCrops as
 select * from crops;
 go
 
-alter view VW_GetAllBins as
+create view VW_GetAllBins as
 select * from bins;
 go
-
-select * from bin_crop
-select * from bins
-truncate table bin_crop
-truncate table bins
-insert into bin_crop values (5, 1000, 1000)
