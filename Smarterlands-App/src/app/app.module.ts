f90d9ba5abd10f7233895dd30d15aede2c09bc33
orymn <ahmed_fabian-16@hotmail.com>
2022-11-14 18:24:42 -0800

    Implementacion ChartJS :ant:

--- Smarterlands-App/src/app/app.module.ts
+++ Smarterlands-App/src/app/app.module.ts
@@ -3,15 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { PrimengModule } from './primeng/primeng.module';
 
 import { AppComponent } from './app.component';
+import { GraficasComponent } from './components/graficas/graficas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GraficasComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [
+    GraficasComponent
+  ]
 })
 export class AppModule { }
