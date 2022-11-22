d25be24e569956585cd439c34c6f77cd6548a17f
orymn <ahmed_fabian-16@hotmail.com>
2022-11-21 22:37:07 -0800

    eduasardo :wedding:  tabla funcional 100% no bonita

--- Smarterlands-App/src/app/services/bins.service.ts
+++ Smarterlands-App/src/app/services/bins.service.ts
@@ -16,22 +16,22 @@ export class BinsService {
     return this.http.get<Bins>(`${this.baseUrl}/bin`)
   }
 
-  postCrop(data: any) {
+  postCrop(data: any, image:string) {
     var formData: any = new FormData();
     formData.append('name', data.name)
     formData.append('description', data.description)
-    formData.append('photo', data.photo)
+    formData.append('photo', image)
     formData.append('optimal_moisture', data.optimal_moisture)
     formData.append('optimal_temperature', data.optimal_temperature)
     return this.http.post<any>(`${this.baseUrl}/crops`, formData);
   }
 
-  putCrop(data:any, data2:any){
+  putCrop(data:any, data2:any, image:string){
     var formData: any = new FormData();
     formData.append('id', data2)
     formData.append('name', data.name)
     formData.append('description', data.description)
-    formData.append('photo', data.photo)
+    formData.append('photo', image)
     formData.append('optimal_moisture', data.optimal_moisture)
     formData.append('optimal_temperature', data.optimal_temperature)
     return this.http.put<any>(`${this.baseUrl}/crops`, formData);
@@ -43,6 +43,10 @@ export class BinsService {
 
   getCrop() {
     return this.http.get<any>(`${this.baseUrl}/crops`)
+  }
+
+  getPhotos(){
+    return this.http.get<any>(`${this.baseUrl}/photos`)
   }
 
 
