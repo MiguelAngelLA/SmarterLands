ba224c7dee8d68649a10152ff155690866a03861
MiguelangelLozAl <miguelangellozanoa@gmail.com>
2022-11-29 22:06:59 -0800

    added db insertion

--- SmarterLands-Back/WS Server/MessageSocket.cs
+++ SmarterLands-Back/WS Server/MessageSocket.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using Microsoft.Net.Http.Headers;
+using Newtonsoft.Json;
 using System.Net.WebSockets;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -13,11 +14,47 @@ namespace websocket_server.Server_Functions
 
         public override async Task Receive(WebSocket webSocket, WebSocketReceiveResult receiveResult, ArraySegment<byte> buffer)
         {
-            var message = @"'" + Encoding.UTF8.GetString(buffer.ToArray(), 0, receiveResult.Count) + "'";
-            await BroadcastMessage(message);
-            //string escapedMessage = Regex.Escape(Encoding.UTF8.GetString(buffer.ToArray(), 0, receiveResult.Count));
-            Console.WriteLine(message);
+            string message = Encoding.UTF8.GetString(buffer.ToArray(), 0, receiveResult.Count);
+            await BroadcastMessage("'" + message + "'");
+            SensorReading sensorReading = JsonConvert.DeserializeObject<SensorReading>(message);
+            sensorReading.Time = new DateTime();
+            string notificationBody = "";
 
+            switch (sensorReading.Notification_id)
+            {
+                case 0:
+                    notificationBody = "Data sent successfully";
+                    break;
+
+                case 1 :
+                    notificationBody = "Warning: incoming storm";
+                    break; 
+
+                case 2 :
+                    notificationBody = "Error: Data malformed!";
+                    break;
+
+                default:
+                    notificationBody = "Unknown Error";
+                    break;
+            }
+
+            SensorReading.Post(
+                sensorReading.Time,
+                sensorReading.Temperature,
+                sensorReading.Humidity,
+                sensorReading.Moisture,
+                sensorReading.Precipitation,
+                sensorReading.Bin_id,
+                notificationBody,
+                (byte)sensorReading.Notification_id);
+
+            
+            // 0 bueno 
+            // 1 warning 
+            // 2 fatal 
+            // default unknown error
+            Console.WriteLine(message);
             //527
         }
 
